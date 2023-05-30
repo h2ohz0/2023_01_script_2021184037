3c9e9b5ca9d959e45825e785f008a73ffb423707
h2ohz0 <hjy04236@naver.com>
2023-05-31 03:10:54 +0900

    드디어 완성..

--- year_month_movielist.py
+++ year_month_movielist.py
@@ -3,6 +3,7 @@ from tkinter import ttk, messagebox
 from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
+import webbrowser
 
 
 movie_details = None
@@ -54,32 +55,50 @@ def get_movie_details(movie_id):
                     break
             break
 
-    return title, poster_path, director, cast, release_date, genres, watch_grade
+    videos_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos"
+    response = requests.get(videos_url, params=params)
+    data = response.json()
+    youtube_key = None
+    for video in data.get('results', []):
+        if video.get('site') == 'YouTube' and video.get('type') == 'Trailer':
+            youtube_key = video.get('key')
+            break
 
+    return title, poster_path, director, cast, release_date, genres, watch_grade, youtube_key
 
 def create_scrollable_list(data, year, month):
     root = Toplevel()
     root.title("Movie List")
-    root.geometry("1000x700")  # Change the size of the window here
+    root.geometry("1100x800")  # Change the size of the window here
+    root.configure(background='#c5d6eb')
 
     movie_count = len(data)
 
-    label_info = Label(root, text=f"Year: {year}   Month: {month}   Movie Count: {movie_count}")
-    label_info.pack(pady=10)
+    label_info = Label(root, text=f"\"{year}년 {month}월\" 개봉 국내 영화 개수: {movie_count}", font=('한컴 말랑말랑 Regular', 18, "bold"))
+    label_info.configure(background='#c5d6eb')
+    label_info.pack(pady = 15)
 
-    scrollbar = Scrollbar(root)
+    frame = Frame(root)
+    frame.pack()
+
+    scrollbar = Scrollbar(frame)
     scrollbar.pack(side=RIGHT, fill=Y)
 
-    listbox = Listbox(root, yscrollcommand=scrollbar.set, width=50)
-    listbox.pack(side=LEFT, fill=BOTH)
+    label_movie_list = Label(frame, text = "개봉 영화",font=('한컴 말랑말랑 Bold',15))
+    label_movie_list.pack()
+
+    listbox = Listbox(frame, yscrollcommand=scrollbar.set, width=40, font=('한컴 말랑말랑 Regular', 15))
+    listbox.pack(side=LEFT, fill=BOTH, padx = 10, pady = 10)
 
     scrollbar.config(command=listbox.yview)
 
-    movie_details = Text(root, width=50)
-    movie_details.pack(side=LEFT)
+    movie_details = Text(root, width=40, font=('한컴 말랑말랑 Regular', 15))
+    movie_details.pack(side=LEFT, padx = 100, pady = 25)
+
 
     label_poster = Label(root)
-    label_poster.pack(side=LEFT)
+    label_poster.pack(side=LEFT, pady =20)
+
 
     def on_movie_select(event):
         index = listbox.curselection()
@@ -91,22 +110,32 @@ def create_scrollable_list(data, year, month):
             movie_details.delete(1.0, END)
 
             movie_details.insert(END,
-                                 f'Title: {movie_info[0]}\nRelease Date: {movie_info[4]}\nDirector: {movie_info[2]}\nCast: {", ".join(movie_info[3])}\nGenres: {", ".join(movie_info[5])}\nWatch Grade: {movie_info[6]}')
+                                 f'<{movie_info[0]}>\n개봉일: {movie_info[4]}\n감독: {movie_info[2]}\n출연: {", ".join(movie_info[3])}\n장르: {", ".join(movie_info[5])}\n관람 등급: {movie_info[6]}')
 
             # Update movie poster
             if movie_info[1]:
                 response = requests.get(f"https://image.tmdb.org/t/p/w200{movie_info[1]}")
                 image = Image.open(BytesIO(response.content))
+
                 photo = ImageTk.PhotoImage(image)
                 label_poster.image = photo  # Keep a reference to the image
                 label_poster.config(image=photo)
             else:
                 label_poster.config(image='')
 
+            if movie_info[7]:  # If there is a Youtube key
+                youtube_url = f"https://www.youtube.com/watch?v={movie_info[7]}"
+                trailer_button.config(command=lambda: webbrowser.open(youtube_url), state=NORMAL,font=('한컴 말랑말랑 Regular', 10))
+            else:
+                # If there is no trailer, disable the button
+                trailer_button.config(command=None, state=DISABLED,font=('한컴 말랑말랑 Regular', 10))
     listbox.bind("<<ListboxSelect>>", on_movie_select)
 
     for item in data:
         listbox.insert(END, item["title"])
+
+    trailer_button = Button(root, text="예고편 보러가기", state=DISABLED,font=('한컴 말랑말랑 Regular', 15))
+    trailer_button.pack(side=LEFT, padx = 50, pady=10)
 
     root.mainloop()
 
@@ -125,12 +154,24 @@ def get_monthly_movie_list(year, month):
         "page": 1
     }
 
-    response = requests.get(base_url, params=params)
-    data = response.json()
+    all_movies = []  # 모든 영화를 저장할 리스트
 
-    movie_list = data["results"]
+    page = 1
+    while True:
+        params["page"] = page
 
-    return movie_list
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        movie_list = data["results"]
+        all_movies.extend(movie_list)  # 현재 페이지의 영화를 전체 리스트에 추가
+
+        # 다음 페이지가 없으면 반복 종료
+        if page >= data["total_pages"]:
+            break
+
+        page += 1
+    return all_movies
 
 
 def create_gui():
@@ -149,17 +190,17 @@ def create_gui():
     root = Tk()
     root.title("Movie List")
 
-    label_year = Label(root, text="Year")
+    label_year = Label(root, text="Year", font = ("한컴 말랑말랑 Bold", 10))
     label_year.grid(row=0, column=0)
     entry_year = Entry(root)
     entry_year.grid(row=0, column=1)
 
-    label_month = Label(root, text="Month")
+    label_month = Label(root, text="Month", font = ("한컴 말랑말랑 Bold", 10))
     label_month.grid(row=1, column=0)
     entry_month = Entry(root)
     entry_month.grid(row=1, column=1)
 
-    button_get_data = Button(root, text="Get Movie List", command=on_button_click)
+    button_get_data = Button(root, text="Get Movie List", font = ("한컴 말랑말랑 Bold", 10), command=on_button_click)
     button_get_data.grid(row=2, column=0, columnspan=2)
 
     root.mainloop()
