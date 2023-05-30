59f8c639496e9e92e9e8770fb5d1c9b458cbb901
h2ohz0 <hjy04236@naver.com>
2023-05-31 03:34:40 +0900

    데이터 엑셀저장까지 구현

--- trend_analysis.py
+++ trend_analysis.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
 from datetime import timedelta, date
+import tkinter.filedialog
+
 
 def get_korean_movie_data(start_year, end_year, start_month, end_month):
     api_key = "191bceef021f24c785530fc8364dcc11"
@@ -45,7 +47,32 @@ def get_korean_movie_data(start_year, end_year, start_month, end_month):
     return movie_count
 
 
-def plot_data(movie_count):
+def plot_data_line(movie_count):
+    years = sorted(list(movie_count.keys()))
+    months = list(range(1, 13))
+    month_labels = [str(month).zfill(2) for month in months]
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    for year in years:
+        counts = [movie_count.get(year, {}).get(month, 0) for month in month_labels]
+        line, = ax.plot(month_labels, counts, marker='o', label=year)
+
+        for i, txt in enumerate(counts):
+            ax.annotate(txt, (month_labels[i], counts[i]),
+                        textcoords="offset points",  # how to position the text
+                        xytext=(0, 10),  # distance from text to points (x,y)
+                        ha='center')  # horizontal alignment can be left, right or center
+
+    ax.set_xlabel("Month")
+    ax.set_ylabel("Number of Movies Released")
+    ax.set_title("Korean Movies - Yearly & Monthly Release Count")
+    ax.legend()
+
+    return fig
+
+
+def plot_data_heatmap(movie_count):
     years = sorted(list(movie_count.keys()))
     months = list(range(1, 13))
     counts = []
@@ -53,7 +80,8 @@ def plot_data(movie_count):
     for year in years:
         year_counts = []
         for month in months:
-            count = movie_count.get(year, {}).get(str(month), 0)
+            month_str = str(month).zfill(2)
+            count = movie_count.get(year, {}).get(month_str, 0)
             year_counts.append(int(count) if int(count) >= 0 else 0)
         counts.append(year_counts)
 
@@ -68,13 +96,46 @@ def plot_data(movie_count):
     ax.set_ylabel("Year")
     ax.set_title("Korean Movies - Yearly & Monthly Release Count")
 
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(years)):
+        for j in range(len(months)):
+            text = ax.text(j, i, counts[i][j], ha="center", va="center", color="black")
+
     cbar = ax.figure.colorbar(im, ax=ax)
     cbar.ax.set_ylabel("Number of Movies Released", rotation=-90, va="bottom")
 
     return fig
 
+def data_to_dataframe(movie_count):
+    years = sorted(list(movie_count.keys()))
+    months = list(range(1, 13))
+    month_labels = [str(month).zfill(2) for month in months]
+    data_dict = {}
+
+    for year in years:
+        counts = [movie_count.get(year, {}).get(month, 0) for month in month_labels]
+        data_dict[year] = counts
+
+    df = pd.DataFrame(data_dict, index=month_labels)
+    df.index.name = "Month"
+    df.columns.name = "Year"
+
+    return df
+def save_data_to_excel(dataframe):
+    file_name = tkinter.filedialog.asksaveasfilename(defaultextension=".xlsx",
+                                                     filetypes=(("Excel file", "*.xlsx"),
+                                                                ("All files", "*.*")))
+    if file_name:
+        dataframe.to_excel(file_name)
+def save_plot(figure):
+    file_name = tkinter.filedialog.asksaveasfilename(defaultextension=".png",
+                                                     filetypes=(("PNG file", "*.png"),
+                                                                ("JPEG file", "*.jpg"),
+                                                                ("All files", "*.*")))
+    if file_name:
+        figure.savefig(file_name)
 def create_gui():
-    global entry_start_year, entry_end_year, entry_start_month, entry_end_month
+    global entry_start_year, entry_end_year, entry_start_month, entry_end_month, var_plot_type
 
     def on_button_click():
         start_year = entry_start_year.get()
@@ -84,13 +145,38 @@ def create_gui():
 
         try:
             movie_count = get_korean_movie_data(start_year, end_year, start_month, end_month)
-            figure = plot_data(movie_count)
+            # Save the movie count data in a global variable so that it can be accessed by save_button_click
+            global current_data
+            current_data = data_to_dataframe(movie_count)
 
-            plt.figure(figure.number)
-            plt.tight_layout()
-            plt.show()
-        except:
+            plot_type = var_plot_type.get()
+            if plot_type == 'Line':
+                figure = plot_data_line(movie_count)
+            elif plot_type == 'Heatmap':
+                figure = plot_data_heatmap(movie_count)
+
+            # Save the figure in a global variable so that it can be accessed by save_button_click
+            global current_figure
+            current_figure = figure
+
+            # Clear the previous plot in the canvas
+            for widget in root.winfo_children():
+                if isinstance(widget, FigureCanvasTkAgg):
+                    widget.get_tk_widget().destroy()
+
+            canvas = FigureCanvasTkAgg(figure, master=root)
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=5, column=3, columnspan=3)
+
+        except Exception as e:
+            print(e)
             messagebox.showerror("Error", "An error occurred while fetching data.")
+
+    def save_button_click():
+        if 'current_data' in globals():
+            save_data_to_excel(current_data)
+        else:
+            messagebox.showinfo("Info", "No data to save. Please get data first.")
 
     root = tk.Tk()
     root.title("Korean Movies - Yearly & Monthly Release Count")
@@ -116,7 +202,19 @@ def create_gui():
     entry_end_month.grid(row=3, column=1)
 
     button_get_data = tk.Button(root, text="Get Data", command=on_button_click)
-    button_get_data.grid(row=4, column=0, columnspan=2)
+    button_get_data.grid(row=4, column=0)
+
+    var_plot_type = tk.StringVar(root)
+    var_plot_type.set("Heatmap")  # default value
+    plot_types = ["Heatmap", "Line"]
+    dropdown = tk.OptionMenu(root, var_plot_type, *plot_types)
+    dropdown.grid(row=4, column=1)
+
+    button_save_plot = tk.Button(root, text="Save Plot", command=save_button_click)
+    button_save_plot.grid(row=4, column=2)
+
+    button_save_data = tk.Button(root, text="Save Data", command=save_button_click)
+    button_save_data.grid(row=4, column=3)
 
     root.mainloop()
 
