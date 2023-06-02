e40979bfa2f8f97dcf405baceda63997acc42dad
h2ohz0 <hjy04236@naver.com>
2023-06-02 14:42:05 +0900

    kofic api로 해당일 국내 박스오피스 출력 명령어 추가

--- newmovie_alarm.py
+++ newmovie_alarm.py
@@ -1,7 +1,10 @@
 import time
 import telepot
 import requests
-from datetime import date
+import urllib.request
+import re
+import json
+from datetime import date, timedelta
 
 TOKEN = '6162134309:AAErOQOG9MK7Bpu3YHjlcMI89qcW6O7L_dE'
 TMDB_API_KEY = '191bceef021f24c785530fc8364dcc11'
@@ -32,6 +35,24 @@ def send_movies_info(chat_id, movies):
         message = f"Title: {title}\nRelease Date: {release_date}\nOverview: {overview}\nLink: {tmdb_url}"
         bot.sendMessage(chat_id, message)
 
+KOFIC_API_KEY = 'f4ebe0c546de8755777b5f9ad9244615'
+
+def get_box_office_kr(targetDt):
+    url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={KOFIC_API_KEY}&targetDt={targetDt}'
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+    boxoffice_data = data.get('boxOfficeResult').get('dailyBoxOfficeList', [])
+    return targetDt, boxoffice_data
+
+def send_box_office_info(chat_id, targetDt, boxoffice_data):
+    message = f'{targetDt}의 국내 박스오피스 TOP 10\n\n'
+    for movie in boxoffice_data:
+        rank = movie.get('rank')
+        title = movie.get('movieNm')
+        audience = movie.get('audiCnt')
+        message += f"{rank}위 - {title}, 관객수: {audience}명\n"
+    bot.sendMessage(chat_id, message)
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type != 'text':
@@ -46,9 +67,12 @@ def handle(msg):
     elif text == '오늘 개봉 영화':
         movies = get_today_release_movies()
         send_movies_info(chat_id, movies)
+    elif re.match(r'^국내 박스오피스 순위 \d{8}$', text):
+        targetDt = text.split(' ')[-1]
+        targetDt, boxoffice_data = get_box_office_kr(targetDt)
+        send_box_office_info(chat_id, targetDt, boxoffice_data)
     else:
-        bot.sendMessage(chat_id, "모르는 명령어입니다. '상영 영화 정보' 또는 '오늘 개봉 영화' 중 하나의 명령을 입력하세요.")
-
+        bot.sendMessage(chat_id, "모르는 명령어입니다. '상영 영화 정보', '오늘 개봉 영화', '국내 박스오피스 순위 YYYYMMDD' 중 하나의 명령을 입력하세요.")
 bot.message_loop(handle)
 
 print('Listening...')
