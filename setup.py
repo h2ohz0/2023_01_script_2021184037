9e626c5aaa3813198e000d19913f899926482ddc
h2ohz0 <hjy04236@naver.com>
2023-06-18 21:40:32 +0900

    setup.py 개발

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='Movie_Trend_Quiz_Alarm',
+    version='1.0',
+    py_modules= ['movie_application','movie_history_quiz', 'newmovie_alarm', 'trend_analysis', 'year_movie_movielist']
+)
